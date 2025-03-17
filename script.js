57d544b1da2b2b71398adebe8fe17dffd6379d91
DavideCotigliani <davide.cotigliani@gmail.com>
2025-03-17 16:31:50 +0100

    [UPD] Added the to.Fixed part to the finalPrice

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //Definisco le variabili
-const prezzoKm = 0.21;
+const priceKm = 0.21;
 let chilometri = prompt("Inserisci il numero di chilometri che vuoi percorrere")
 let age = prompt ("Inserisci l'età del passeggero")
-
 // prezzo base senza scontistiche
-let price = chilometri * prezzoKm;
+let price = chilometri * priceKm;
 
 // prezzo finale
 let finalPrice
@@ -16,12 +15,12 @@ if (age < 18) {
 
 // seconda scontistica 40%
 else if ( age > 65) {
-    finalprice = price - price * 40 / 100
+    finalPrice = price - price * 40 / 100
 }
 
 // prezzo FINALE senza sconti
 else{
     finalPrice = price;
 }
-// per visualizzare il prezzo finale
-console.log(`Il prezzo è di ${finalPrice}`)
+// per visualizzare il prezzo finale con i decimali
+console.log(`Il prezzo è di ${finalPrice.toFixed(2)}`)
